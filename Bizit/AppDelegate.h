14e28a75946a40bdd8b25ba1ca4a9b84a5e0674f
Apple <Apple@Apples-MacBook-Pro.local>
2015-06-16 09:51:02 +0530

    Initial Commit

---
+++ Bizit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bizit
+//
+//  Created by Apple on 16/06/15.
+//  Copyright (c) 2015 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
